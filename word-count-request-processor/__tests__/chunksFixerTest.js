a7cec1b528d07569227782cea388d8061a6dbab6
Omer Baki <omer@soluto.com>
2020-01-22 11:59:29 +0200

    Add unit tests for chunksFixer

--- word-count-request-processor/__tests__/chunksFixerTest.js
+++ word-count-request-processor/__tests__/chunksFixerTest.js
@@ -1,6 +1,19 @@
 const ChunksFixer = require('../src/chunksFixer');
 
-test('Testing 1234...', () => {
+test('Test init state', () => {
     const chunksFixer = new ChunksFixer();
     expect(chunksFixer.getPrevChunkLastWord()).toBe('');
+});
+
+test('Fix chunk should remove last word and concat to the beginning next chunk', () => {
+    const testChunks = ["This is a chunk exa", "mple to check"];
+    const chunksFixer = new ChunksFixer();
+
+    const firstChunk = chunksFixer.fixLastWord(testChunks[0]);
+    expect(firstChunk).toBe('This is a chunk');
+
+    const secondChunk = chunksFixer.fixLastWord(testChunks[1]);
+    expect(secondChunk).toBe('example to');
+
+    expect(chunksFixer.getPrevChunkLastWord()).toBe('check');
 });
